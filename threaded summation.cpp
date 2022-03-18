186b599b5ba03288226427acca58edf085b6b13a
poseidon-code <pritamhalder05062000@gmail.com>
2022-03-18 11:00:00 +0530

    update: indentations

--- threaded summation.cpp
+++ threaded summation.cpp
@@ -11,31 +11,30 @@ int sum[4] = {0, 0, 0, 0};
 int part = 0;
 
 
-void *sum_array(void *arg)
-{	
+void *sum_array(void *arg) {	
 	int thread_part = part++;
 	for(int i = thread_part*(MAX/4); i < (thread_part+1)*(MAX/4); i++)
-	{
 		sum[thread_part] += a[i];
-	}
 	return NULL;
 }
 
 
-int main()
-{
+int main() {
 	pthread_t threads[MAX_THREAD];
+	
 	for(int i=0; i<MAX_THREAD; i++)
 		pthread_create(&threads[i], NULL, sum_array, (void*)NULL);
+	
 	for(int i=0; i<MAX_THREAD; i++)
 		pthread_join(threads[i], NULL);
 	
 	int total_sum = 0;
-	for(int i=0; i<MAX_THREAD; i++)
-	{
+	for(int i=0; i<MAX_THREAD; i++) {
 		cout<<"SUM of THREAD ["<<i<<"] = "<<sum[i]<<"\n";
 		total_sum += sum[i];
 	}
+
 	cout<<"TOTAL SUM : "<<total_sum<<endl;
+	
 	return 0;
 }
