9e07183903a6666d8480cbe9bbd2d39ac9c650fd
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2023-04-21 10:00:00 +0530

    update: implement datarate based streaming

--- SocketTransmitter/SocketTransmitter.cpp
+++ SocketTransmitter/SocketTransmitter.cpp
@@ -3,16 +3,31 @@
 #include <ctime>
 #include <iostream>
 #include <thread>
+#include <atomic>
 
 #include "Socket.h"
 
+#define CL "\r\033[K"       // clear terminal line
+#define DATARATE 200        // data rate (in miliseconds)
 
 
-volatile sig_atomic_t rSIGINT = 0;
+// global signal variables
+volatile sig_atomic_t sSIGINT = 0;
 
 void handleInterrupt(int signal) {
     switch (signal) {
-        case SIGINT: rSIGINT = 1; break;
+        case SIGINT: sSIGINT = 1; break;
+    }
+}
+
+
+std::atomic<unsigned long> tick{0};
+void tickThread(std::atomic<unsigned long>& tick) {
+    while (!sSIGINT) {
+        tick.store(
+            std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count(),
+            std::memory_order_relaxed
+        );
     }
 }
 
@@ -20,20 +35,30 @@ void handleInterrupt(int signal) {
 int main() {
     signal(SIGINT, handleInterrupt);
 
+    unsigned long previousTick = 0, currentTick = 0;
+    std::thread oTickThread(tickThread, std::ref(tick));
+
     Socket* socket = new Socket("127.0.0.1", 8000);
     socket->Create();
 
-    const int dataRate_ms = 500;
-    unsigned long long int streamedDataCount = 0;
+    int c = 0;
 
-    while (!rSIGINT) {
-        const char *data = "Hello, client!";
-        socket->Send(data);
-        
-        std::cout << "Data Streamed : " << ++streamedDataCount << "\n";
-        std::this_thread::sleep_for(std::chrono::milliseconds(dataRate_ms));
-    }
+    while (!sSIGINT) {
+        currentTick = tick.load(std::memory_order_relaxed);
+
+        if (currentTick >= previousTick + DATARATE) {
+            previousTick = currentTick;
+
+            std::cout << CL << "Data Sent : " << ++c << std::flush;
+
+            const char *data = "Hello, client!";
+            socket->Send(data);
+        }
+    } std::cout << std::endl;
+
 
     socket->Close();
+    oTickThread.join();
+
     return 0;
 }
