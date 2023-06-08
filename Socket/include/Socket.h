3f4ada7d719c4de5e02d76a99b3c55b8550e6cc6
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2023-06-08 10:00:00 +0530

    update: socket library

--- Socket/include/Socket.h
+++ Socket/include/Socket.h
@@ -4,17 +4,24 @@
 #include <functional>
 
 
+struct Network {
+    sockaddr_in address{};
+    socklen_t length;
+    Network(const char* ip, const unsigned short int port);
+};
+
 class Socket {
 private:
     sockaddr_in address{};
+    socklen_t length;
 
 public:
     int udpsocket;
-    Socket(const char* ccIP, unsigned short usPort);
+    Socket(const Network& network);
     ~Socket();
 
-    int Send(const char* ccData, unsigned short usSendToPort);
-    int Receive(std::function<void(const char*, int)> fnCallback, const unsigned short usBufferSize);
+    int Send(const char* ccData, const Network& network);
+    int Receive(std::function<void(const char*, int)> fnCallback, const unsigned short int cusBufferSize);
 };
 
-extern "C" Socket* Constructor(const char* ccIP, unsigned short usPort);
+extern "C" Socket* Constructor(const Network& network);
